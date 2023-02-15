5eb7e78eda58552a1ecf22fad06b528e436565af
eastrain <122263021+kwakdongwoo@users.noreply.github.com>
2023-02-15 16:35:24 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -40,3 +40,39 @@ if ($('html').hasClass('mobile')) {
   })
 
 }
+
+$('#scroll_btn_group .main').on('click',function(){
+
+  if ( $(this).parents().hasClass('on') ) {
+    $(this).parents().removeClass('on')
+  } else {
+    $(this).parents().addClass('on')
+  }
+
+  if ($(this).find('i').hasClass('fa-bars-staggered') ) {
+    $(this).find('i').removeClass('fa-bars-staggered').addClass('fa-x')
+  } else {
+    $(this).find('i').removeClass('fa-x').addClass('fa-bars-staggered')
+  }
+  
+
+})
+
+
+$('#scroll_btn_group').hide()
+
+$(window).scroll(function(){
+
+	if ($(this).scrollTop() > 300){
+		$('#scroll_btn_group').show();
+	} else{
+		$('#scroll_btn_group').hide().removeClass('on');
+    $('#scroll_btn_group .main').find('i').removeClass('fa-x').addClass('fa-bars-staggered')
+	}
+
+})
+
+$('#scroll_btn_group .sub1').click(function(){
+	$('html, body').animate({scrollTop:0},400);
+	return false;
+})
--- js/index.js
+++ js/index.js
@@ -1,12 +1,74 @@
-$('.slide_group').slick({
-  autoplay: true,
-  autoplaySpeed: 3000,
-  dots: true,
-  vertical: true,
-  prevArrow: '<button class="slick-prev slick-arrow"><i class="fa-solid fa-chevron-up"></i></button>',
-  nextArrow: '<button class="slick-next slick-arrow"><i class="fa-solid fa-chevron-down"></i></button>',
-  pauseOnHover: false,
-})
+var ww = $(window).width()
+if (ww > 1024) {
+  $('html').addClass('pc')
+} else {
+  $('html').addClass('mobile')
+}
+
+
+
+
+if ($('html').hasClass('pc')) {
+
+  $('.slide_group').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    vertical: true,
+    prevArrow: '<button class="slick-prev slick-arrow"><i class="fa-solid fa-chevron-up"></i></button>',
+    nextArrow: '<button class="slick-next slick-arrow"><i class="fa-solid fa-chevron-down"></i></button>',
+    pauseOnHover: false,
+  })
+
+  $('.slide_group2').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 6,
+    centerMode: true,
+    arrows: false,
+    dots: false,
+    draggable:true,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 2,
+        centerPadding: '100px',
+      }
+    }]
+  })
+
+} else if ($('html').hasClass('mobile')) {
+
+  $('.slide_group').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    arrows:false,
+    vertical: false,
+    pauseOnHover: false,
+  })
+
+  $('.slide_group2').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 2,
+    centerMode: true,
+    arrows: false,
+    dots: false,
+    draggable:true,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 2,
+        centerPadding: '10px',
+      }
+    }]
+  })
+
+
+}
 
 
 $('#article7 .qna .open').on('click', function () {
@@ -15,7 +77,7 @@ $('#article7 .qna .open').on('click', function () {
 
   $(this).parent().siblings().find('i').removeClass('fa-minus').addClass('fa-plus')
   $(this).parent().siblings().find('span').slideUp()
-  
+
   if ($(this).find('i').hasClass('fa-plus')) {
     $(this).find('i').removeClass('fa-plus').addClass('fa-minus')
   } else {
